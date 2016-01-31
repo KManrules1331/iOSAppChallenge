ba501809f2b5d7279e8beabfae5b1a683d00ffa7
KManrules1331 <klarson1331@gmail.com>
2016-01-31 18:24:55 -0500

    Added game states
    Game states lol

---
+++ tvOS_App/tvOS_App/GameStateEnum.swift
@@ -0,0 +1,16 @@
+//
+//  GameStateEnum.swift
+//  tvOS_App
+//
+//  Created by Apple on 1/31/16.
+//  Copyright © 2016 PumpkinSpiceGirls. All rights reserved.
+//
+
+import Foundation
+
+enum GameStateEnum {
+    case Menu
+    case Running
+    case Paused
+    case GameOver
+}
